1b45f1ca4dc9d48bd91b2efb6ca23a615bea46f1
Nicolas Brax <nicolas.brax.external@airbus.com>
2018-01-11 10:51:05 +0100

    Git Creation

---
+++ src/rpgscenariocreation/RPGScenarioCreation.java
@@ -0,0 +1,17 @@
+package rpgscenariocreation;
+
+/**
+ * This project is dedicated to Scenario creation for several tabletop RPG.
+ * Will ideally include some learning to adapt to user preference.
+ * @author nicolas
+ */
+public class RPGScenarioCreation {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+
+  }
+  
+}
