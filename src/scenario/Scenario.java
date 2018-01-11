1ac8c2c4c2679b8c2a02f590f3bf4277b8f3e097
Nicolas Brax <nicolas.brax.external@airbus.com>
2018-01-11 17:00:11 +0100

    WIP

---
+++ src/scenario/Scenario.java
@@ -0,0 +1,9 @@
+package scenario;
+
+/**
+ * A scenario as built by the engine. 
+ * @author nicolas
+ */
+public class Scenario {
+  
+}
---
+++ src/scenario/ScenarioEngine.java
@@ -0,0 +1,10 @@
+package scenario;
+
+/**
+ * Given the specific of a Universe, build a scenario.
+ * Also has the ability to learn building preferences.
+ * @author nicolas
+ */
+public class ScenarioEngine {
+  
+}
